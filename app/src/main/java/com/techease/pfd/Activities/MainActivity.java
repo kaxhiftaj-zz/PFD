8e91ce8157623f7a1a0d773d999cba5d594bf359
DeveloperAdam <adamnooriit@gmail.com>
2017-11-20 16:58:55 +0500

    change in xml

--- app/src/main/java/com/techease/pfd/Activities/MainActivity.java
+++ app/src/main/java/com/techease/pfd/Activities/MainActivity.java
@@ -6,24 +6,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.CallbackManager;
 import com.techease.pfd.R;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnFb,btnEmail;
-
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        callbackManager = CallbackManager.Factory.create();
         btnFb=(Button)findViewById(R.id.btnFb);
         btnFb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-        startActivity(new Intent(MainActivity.this,PFD.class));
-                finish();
+                // Callback registration
+
             }
         });
         btnEmail=(Button)findViewById(R.id.btnEmail);
